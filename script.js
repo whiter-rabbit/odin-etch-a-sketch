9cb8497a0c7496765f8448a52c1fe15253557c5a
whiter-rabbit <126683095+whiter-rabbit@users.noreply.github.com>
2023-08-04 19:13:01 +0200

    Start adding hover/click functionality for grid
    Do not work properly yet with eventListener - targets the last
    cell only.
    But works with :hover pseudo-selector in css
    Regarding prompt input - the user's grid size: added evening a number
    to an integer and limiting the number between 2 and 100.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 console.log("test")
 
 const gridGrandparent = document.querySelector(".grid-grandparent");
+// let gridParent = document.querySelector(".grid-parent");
+// let gridCell = document.querySelector(".grid-cell");
 const gridSizeBtn = document.querySelector(".grid-size-btn");
-let gridParent;
 
 let gridSizeChosen = 16;
 createGrid();
@@ -11,24 +12,69 @@ function createGrid (){
     gridParent = document.createElement("div");
     gridParent.classList.add("grid-parent");
     gridGrandparent.append(gridParent);
-    let gridCell; 
     for (let i=0; i<(gridSizeChosen ** 2); i++) {
         gridCell = document.createElement("div");
         gridCell.classList.add("grid-cell");
         gridParent.append(gridCell);
-        gridCell.style.setProperty('--cell-size', (961 - gridSizeChosen) / gridSizeChosen + "px");
-        }
+        //INFO-below: adding property for css (--cell-size) that calculates the size of the cell based on the number chosen by the user
+        gridCell.style.setProperty('--cell-size', (961 - gridSizeChosen) / gridSizeChosen + "px");      
+    }
 }
 
 gridSizeBtn.addEventListener('click', function(e){
     playerSelection();
 });
 
-
 function playerSelection () {
     gridParent.remove();
-    gridSizeChosen = Number(prompt("Choose the desired size of the square grid - type an integer between 2 and 100"));
-    createGrid()
+    gridSizeChosen = Math.floor(Number(prompt("Choose the desired size of the square grid - type an integer between 2 and 100")));
+    if (gridSizeChosen < 2 || gridSizeChosen > 100){
+        alert("The number should be between 2 and 100");
+        gridSizeChosen = 16;
+        createGrid();
+    }
+    else{
+        createGrid();
+        }
 }
 
+//INFO: not working yet
+let gridCells = Array.from(document.querySelectorAll(".grid-cell"));
+// i should probably access all elements of an array
+for (let i=0; i<(gridSizeChosen ** 2); i++) {
+    gridCell.addEventListener("click", function(e){
+    console.log("hover");
+    gridCell.classList.add("grid-cell-hover");
+})
+};
 
+
+// // INFO: below version that works only for the last cell:
+// gridCell.addEventListener("click", function(e){
+//     console.log("hover");
+//     gridCell.classList.add("grid-cell-hover");
+// })
+
+
+
+// // INFO: below random not working stuff
+// }
+// if (typeof gridSizeChosen ==="NaN"){
+//     console.log("NaN");
+//     alert("This is not a number - try again")
+// }
+// if (gridSizeChosen === ""){
+//     console.log("empty")
+//     alert("Empty")
+// }
+
+// }
+// if(prompt.value == " "){
+//     console.log("empty")
+// }
+
+// if (Number.gridSizeChosen === "") {
+        //     alert("Please enter a number");
+        //     gridSizeChosen = 16;
+        //     createGrid();
+        // }
