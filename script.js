70ebf4a5c814c4ffe981edd35d3a293b1f9d4e0e
whiter-rabbit <126683095+whiter-rabbit@users.noreply.github.com>
2023-08-16 16:16:54 +0200

    Add conditions for user's prompt

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 console.log("Play the game")
 
 const gridGrandparent = document.querySelector(".grid-grandparent");
-let gridParent = document.querySelector(".grid-parent");
 let gridCell = document.querySelector(".grid-cell");
 const gridSizeBtn = document.querySelector(".grid-size-btn");
 
@@ -19,7 +18,7 @@ function createGrid (){
         gridCell.classList.add("grid-cell");
         gridParent.append(gridCell);
         //INFO-below: adding property for css (--cell-size) that calculates the size of the cell based on the number chosen by the user
-        gridCell.style.setProperty('--cell-size', (961 - gridSizeChosen) / gridSizeChosen + "px"); 
+        gridCell.style.setProperty('--cell-size', (900 + 1 - gridSizeChosen) / gridSizeChosen + "px"); 
     }
 }
 
@@ -31,15 +30,39 @@ gridSizeBtn.addEventListener('click', function(e){
 function playerSelection () {
     gridParent.remove();
     gridSizeChosen = Math.floor(Number(prompt("Choose the desired size of the square grid - type an integer between 2 and 100")));
-    if (gridSizeChosen < 2 || gridSizeChosen > 100){
-        alert("The number should be between 2 and 100");
+    if (Number.isNaN(gridSizeChosen)) {
+        alert("This is not a number. Please type a number between 2 and 100");
         gridSizeChosen = 16;
         createGrid();
     }
-    else{
-        createGrid();     
+    else {
+        if (gridSizeChosen < 2 || gridSizeChosen > 100){
+            if (gridSizeChosen == 0 && gridSizeChosen !== " ") {
+                alert("Please type a number between 2 and 100"); 
+                gridSizeChosen = 16;
+                createGrid();
+            }
+            else {
+                alert("The number should be between 2 and 100");
+                gridSizeChosen = 16;
+                createGrid();
+            }
         }
+        else {
+            createGrid();     
+            }
+    }
+//     if (gridSizeChosen < 2 || gridSizeChosen > 100) {
+//         alert("Please type a number between 2 and 100"); 
+//         gridSizeChosen = 16;
+//         createGrid();
+//     }
+//     else {
+//         createGrid();   
+//     }
 }
+
+
 
 
 function gridMark () {
@@ -52,28 +75,3 @@ for (let i=0; i<(gridSizeChosen ** 2); i++) {
 }
 
 gridMark();
-
-
-
-
-// // INFO: below random not working stuff
-// }
-// if (typeof gridSizeChosen ==="NaN"){
-//     console.log("NaN");
-//     alert("This is not a number - try again")
-// }
-// if (gridSizeChosen === ""){
-//     console.log("empty")
-//     alert("Empty")
-// }
-
-// }
-// if(prompt.value == " "){
-//     console.log("empty")
-// }
-
-// if (Number.gridSizeChosen === "") {
-        //     alert("Please enter a number");
-        //     gridSizeChosen = 16;
-        //     createGrid();
-        // }
